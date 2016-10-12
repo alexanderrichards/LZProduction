8a7c247df819cea863654afc1b28698f599cdba8
Alexander Richards <a.richards@imperial.ac.uk>
2016-10-12 15:37:26 +0100

    Add cert checking on the webserver.
    Add certificate checking on the webserver. Also store the unique DN/CA combination in the db.
    
    	modified:   scripts/start-webserver.py
    	new file:   src/python/CertWebServer.py
    	modified:   src/python/services/RequestsDB.py

---
+++ src/python/CertWebServer.py
@@ -0,0 +1,46 @@
+""""""
+import os
+import cherrypy
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy_utils import SQLTableBase, create_db, db_session
+from suds_utils import CertClient
+
+class Users(SQLTableBase):
+    """Users SQL Table."""
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    dn = Column(String(250), nullable=False)
+    ca = Column(String(250), nullable=False)
+    suspended = Column(Boolean(), nullable=False)
+    admin = Column(Boolean(), nullable=False)
+
+class CertWebServer(object):
+    """The Web server."""
+
+    def __init__(self, dburl, index_page):
+        """Initialisation."""
+        self.dburl = dburl        
+        create_db(dburl)
+        self.index_page = index_page
+
+    @cherrypy.expose
+    def index(self):
+        """Return the index page."""
+        clientDN = os.environ['Ssl-Client-S-Dn']
+        clientCA = os.environ['Ssl-Client-I-Dn']
+        clientVerified = os.environ['Ssl-Client-Verify']
+        if clientVerified != 'SUCCESS':
+            return '401 Unauthorized: Cert not verified.'
+
+        with db_session(self.dburl) as session:
+            users = session.query(Users).filter(Users.dn == clientDN).filter(Users.ca == clientCA).all()
+            if not users:
+                return '403 Forbidden: Unknown user'
+            if len(users) > 1:
+                return '500 Internal Server Error: Duplicate user detected.'
+            if user[0].suspended:
+                return '403 Forbidden: User is suspended by VO'
+
+        with open(self.index_page, 'rb') as front_page:
+            return front_page.read()

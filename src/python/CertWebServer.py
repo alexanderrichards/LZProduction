f21b6c56aea4aa357c235e8af6f47e9aa0eb86e7
Alexander Richards <a.richards@imperial.ac.uk>
2016-10-13 13:31:05 +0100

    Convert Apache client cert.
    Convert client cert from Apache comma separated style to more normal slash separated style. Also added some style fixes and a typo fix.
    
    	modified:   src/python/CertWebServer.py

--- src/python/CertWebServer.py
+++ src/python/CertWebServer.py
@@ -1,9 +1,8 @@
-""""""
-import os
+"""Certificate authenticated web server."""
 import cherrypy
 from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy_utils import SQLTableBase, create_db, db_session
-from suds_utils import CertClient
+from .sqlalchemy_utils import SQLTableBase, create_db, db_session
+
 
 class Users(SQLTableBase):
     """Users SQL Table."""
@@ -15,20 +14,41 @@ class Users(SQLTableBase):
     suspended = Column(Boolean(), nullable=False)
     admin = Column(Boolean(), nullable=False)
 
+
+def apache_client_convert(dn, ca=None):
+    """
+    Convert Apache style client certs.
+
+    Convert from the Apache comma delimeted style to the
+    more usual slash delimeted style.
+
+    Args:
+        dn (str): The client DN
+        ca (str): [Optional] The client CA
+
+    Returns:
+        tuple: The converted client (DN, CA)
+    """
+    dn = '/' + '/'.join(reversed(dn.split(',')))
+    if ca is not None:
+        ca = '/' + '/'.join(reversed(ca.split(',')))
+    return dn, ca
+
+
 class CertWebServer(object):
     """The Web server."""
 
     def __init__(self, dburl, index_page):
         """Initialisation."""
-        self.dburl = dburl        
+        self.dburl = dburl
         create_db(dburl)
         self.index_page = index_page
 
     @cherrypy.expose
     def index(self):
         """Return the index page."""
-        clientDN = cherrypy.request.headers['Ssl-Client-S-Dn']
-        clientCA = cherrypy.request.headers['Ssl-Client-I-Dn']
+        clientDN, clientCA = apache_client_convert(cherrypy.request.headers['Ssl-Client-S-Dn'],
+                                                   cherrypy.request.headers['Ssl-Client-I-Dn'])
         clientVerified = cherrypy.request.headers['Ssl-Client-Verify']
         if clientVerified != 'SUCCESS':
             return '401 Unauthorized: Cert not verified for user DN: %s, CA: %s.' % (clientDN, clientCA)
@@ -39,7 +59,7 @@ class CertWebServer(object):
                 return '403 Forbidden: Unknown user: (%s, %s), users: %s' % (clientDN, clientCA, users)
             if len(users) > 1:
                 return '500 Internal Server Error: Duplicate user detected. users: %s' % users
-            if user[0].suspended:
+            if users[0].suspended:
                 return '403 Forbidden: User is suspended by VO'
 
         with open(self.index_page, 'rb') as front_page:

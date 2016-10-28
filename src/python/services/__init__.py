823ee303d21ce8933597cb54e16fbbedce225aed
Alexander Richards <a.richards@imperial.ac.uk>
2016-10-28 11:53:04 +0100

    Add docstring to services __init__.py.
    	modified:   src/python/services/__init__.py

--- src/python/services/__init__.py
+++ src/python/services/__init__.py
@@ -1,3 +1,8 @@
+"""
+Initialise module.
+
+Load all services at module scope.
+"""
 from .CVMFSAppVersions import CVMFSAppVersions
 from .GitTagMacros import GitTagMacros
 from .RequestsDB import RequestsDB

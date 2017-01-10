de10bba4fba739d56f97588e01d23c29b945f325
Alexander Richards <a.richards@imperial.ac.uk>
2017-01-10 13:12:00 +0000

    Make the webserver a daemon.

--- scripts/start-webserver.py
+++ scripts/start-webserver.py
@@ -7,66 +7,13 @@ import argparse
 import importlib
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from functools import partial
 import cherrypy
 import jinja2
+from daemonize import Daemonize
 
-
-if __name__ == '__main__':
-    lzprod_root = os.path.dirname(
-        os.path.dirname(
-            os.path.expanduser(
-                os.path.expandvars(
-                    os.path.realpath(
-                        os.path.abspath(__file__))))))
-
-    parser = argparse.ArgumentParser(description='Run the LZ production web server.')
-    parser.add_argument('-v', '--verbose', default=logging.INFO, action="store_const",
-                        const=logging.DEBUG, dest='logginglevel',
-                        help="Increase the verbosity of output")
-    parser.add_argument('-l', '--log-dir', default=os.path.join(lzprod_root, 'log'),
-                        help="Path to the log directory. Will be created if doesn't exist "
-                             "[default: %(default)s]")
-    parser.add_argument('-r', '--git-repo', default='git@lz-git.ua.edu:sim/TDRAnalysis.git',
-                        help="Git repo url [default: %(default)s]")
-    parser.add_argument('-g', '--git-dir', default=os.path.join(lzprod_root, 'git', 'TDRAnalysis'),
-                        help="Path to the directory where to clone TDRAnalysis git repo "
-                             "[default: %(default)s]")
-    parser.add_argument('-d', '--dburl',
-                        default="sqlite:///" + os.path.join(lzprod_root, 'requests.db'),
-                        help="URL for the requests DB. Note can use the prefix 'mysql+pymysql://' "
-                             "if you have a problem with MySQLdb.py [default: %(default)s]")
-    parser.add_argument('-a', '--socket-host', default='0.0.0.0',
-                        help="The host address to listen on (0.0.0.0 means all available) "
-                             "[default: %(default)s]")
-    parser.add_argument('-p', '--socket-port', default=8080, type=int,
-                        help="The host port to listen on [default: %(default)s]")
-    parser.add_argument('-t', '--thread-pool', default=8, type=int,
-                        help="The number of threads in the pool [default: %(default)s]")
-    args = parser.parse_args()
-
-    if not os.path.isdir(args.log_dir):
-        if os.path.exists(args.log_dir):
-            raise Exception("%s path already exists and is not a directory so cant make log dir" % args.log_dir)
-        os.mkdir(args.log_dir)
-
-    fhandler = TimedRotatingFileHandler(os.path.join(args.log_dir, 'LZWebServer.log'),
-                                        when='midnight', backupCount=5)
-    fhandler.setFormatter(logging.Formatter("[%(asctime)s] %(name)15s : %(levelname)8s : %(message)s"))
-    root_logger = logging.getLogger()
-    root_logger.addHandler(fhandler)
-    root_logger.setLevel(args.logginglevel)
-
-    # force cherrypy to log to the logfile
-    cherrypy_logger = logging.getLogger('cherrypy.error')
-    cherrypy_logger.setLevel(logging.NOTSET)
-    handlers = cherrypy_logger.handlers[:]
-    for h in handlers:
-        cherrypy_logger.removeHandler(h)
-
-    logger = logging.getLogger("LZWebServer")
-    logger.debug("Script called with args: %s", args)
-#    sys.exit(0)
-
+def daemon_main(args):
+    """Main function executed by the daemon process."""
     # Add the python src path to the sys.path for future imports
     sys.path = [os.path.join(lzprod_root, 'src', 'python')] + sys.path
 
@@ -112,4 +59,75 @@ if __name__ == '__main__':
     cherrypy.engine.start()
     cherrypy.engine.block()
 
-    logging.shutdown()
+
+
+if __name__ == '__main__':
+    lzprod_root = os.path.dirname(
+        os.path.dirname(
+            os.path.expanduser(
+                os.path.expandvars(
+                    os.path.realpath(
+                        os.path.abspath(__file__))))))
+
+    parser = argparse.ArgumentParser(description='Run the LZ production web server.')
+    parser.add_argument('-v', '--verbose', default=logging.INFO, action="store_const",
+                        const=logging.DEBUG, dest='logginglevel',
+                        help="Increase the verbosity of output")
+    parser.add_argument('-l', '--log-dir', default=os.path.join(lzprod_root, 'log'),
+                        help="Path to the log directory. Will be created if doesn't exist "
+                             "[default: %(default)s]")
+    parser.add_argument('-r', '--git-repo', default='git@lz-git.ua.edu:sim/TDRAnalysis.git',
+                        help="Git repo url [default: %(default)s]")
+    parser.add_argument('-g', '--git-dir', default=os.path.join(lzprod_root, 'git', 'TDRAnalysis'),
+                        help="Path to the directory where to clone TDRAnalysis git repo "
+                             "[default: %(default)s]")
+    parser.add_argument('-d', '--dburl',
+                        default="sqlite:///" + os.path.join(lzprod_root, 'requests.db'),
+                        help="URL for the requests DB. Note can use the prefix 'mysql+pymysql://' "
+                             "if you have a problem with MySQLdb.py [default: %(default)s]")
+    parser.add_argument('-a', '--socket-host', default='0.0.0.0',
+                        help="The host address to listen on (0.0.0.0 means all available) "
+                             "[default: %(default)s]")
+    parser.add_argument('-p', '--socket-port', default=8080, type=int,
+                        help="The host port to listen on [default: %(default)s]")
+    parser.add_argument('-t', '--thread-pool', default=8, type=int,
+                        help="The number of threads in the pool [default: %(default)s]")
+    parser.add_argument('-i', '--pid-file', default=os.path.join(lzprod_root, 'webserver-daemon.pid'),
+                        help="The pid file used by the daemon [default: %(default)s]")
+    parser.add_argument('--debug-mode', action='store_true', default=False,
+                        help="Run the daemon in a debug interactive monitoring mode. "
+                             "(debugging only)")
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.log_dir):
+        if os.path.exists(args.log_dir):
+            raise Exception("%s path already exists and is not a directory so cant make log dir" % args.log_dir)
+        os.mkdir(args.log_dir)
+
+    fhandler = TimedRotatingFileHandler(os.path.join(args.log_dir, 'LZWebServer.log'),
+                                        when='midnight', backupCount=5)
+    if args.debug_mode:
+        fhandler = logging.StreamHandler()
+    fhandler.setFormatter(logging.Formatter("[%(asctime)s] %(name)15s : %(levelname)8s : %(message)s"))
+    root_logger = logging.getLogger()
+    root_logger.addHandler(fhandler)
+    root_logger.setLevel(args.logginglevel)
+
+    # force cherrypy to log to the logfile
+    cherrypy_logger = logging.getLogger('cherrypy.error')
+    cherrypy_logger.setLevel(logging.NOTSET)
+    handlers = cherrypy_logger.handlers[:]
+    for h in handlers:
+        cherrypy_logger.removeHandler(h)
+
+    logger = logging.getLogger("LZWebServer")
+    logger.debug("Script called with args: %s", args)
+#    sys.exit(0)
+
+
+    daemon = Daemonize(app=os.path.splitext(os.path.basename(__file__))[0],
+                       pid=args.pid_file,
+                       keep_fds=[fhandler.stream.fileno()],
+                       foreground=args.debug_mode,
+                       action=partial(daemon_main, args=args))
+    daemon.start()

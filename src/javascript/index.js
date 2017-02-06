23779b6f825f157cfa24968638d5b368f7970bd2
Alexander Richards <a.richards@imperial.ac.uk>
2017-02-06 15:15:29 +0000

    Sort by request id decending after submission date.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
                                       return json.data;
                                   }
                                  },
-                           order: [[4, "asc"], [5, "desc"]],
+                           order: [[4, "asc"], [5, "desc"], [1, "desc"]],
                            autoWidth: false,
                            columnDefs: [{ targets: "_all", className: "dt-body-left dt-head-left",
                                           render: function(data, type, row){

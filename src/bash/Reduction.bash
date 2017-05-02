e2fea244db758bac1e38d921cfa6b1086fecb695
Alexander Richards <inferno_alex@hotmail.com>
2017-05-02 16:17:18 +0100

    Add reduction script. (#101)
    Add reduction script.

--- src/bash/Reduction.bash
+++ src/bash/Reduction.bash
@@ -1,11 +1,18 @@
-LIBNEST_DIR=/cvmfs/lz.opensciencegrid.org/fastNEST/release-$libnest_version
-REDUCTION_DIR=/cvmfs/lz.opensciencegrid.org/TDRAnalysis/release-$reduction_version
+LIBNEST_DIR=/cvmfs/lz.opensciencegrid.org/fastNEST/release-{{ libnest_version }}
+REDUCTION_DIR=/cvmfs/lz.opensciencegrid.org/TDRAnalysis/release-{{ reduction_version }}
 
 # reduce and then copy both to our central storage.
 source $LIBNEST_DIR/libNEST/thislibNEST.sh
 REDUCTION_OUTPUT_FILE=$(basename $SIM_OUTPUT_FILE .root)_analysis_tree.root
-$REDUCTION_DIR/ReducedAnalysisTree/LZSim2AnalysisTree $SIM_OUTPUT_FILE $REDUCTION_OUTPUT_FILE
 
+
+{% if app == 'BACCARAT' %}
+export BACC_TOOLS=/cvmfs/lz.opensciencegrid.org/BACCARAT/release-1.0.0/tools
+export LD_LIBRARY_PATH=$BACC_TOOLS:$LD_LIBRARY_PATH
+$REDUCTION_DIR/ReducedAnalysisTree/Bacc2AnalysisTree $SIM_OUTPUT_FILE $REDUCTION_OUTPUT_FILE
+{% else %}
+$REDUCTION_DIR/ReducedAnalysisTree/LZSim2AnalysisTree $SIM_OUTPUT_FILE $REDUCTION_OUTPUT_FILE
+{% endif %}
 ## FILE UPLOAD
 ######################################################################
 #REDUCED_STORE_PATH=$DATA_STORE_PATH/LUXSim_$(basename $LUXSIM_DIR)_$G4VER/reduced_v$reduction_version/$(basename $MACRO_FILE _parametric.mac)

cce311b75a27f63402f72977507600bd07fa37b4
requires.io <support@requires.io>
2020-02-08 03:25:27 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=['CherryPy==18.5.0',
                       'daemonize==2.5.0',
                       'enum34==1.1.6',
-                      'GitPython==3.0.5',
+                      'GitPython==3.0.6',
                       'html==1.16',
                       'jinja2==2.11.1',
                       'natsort==7.0.1',

b4f8b0695bb46cfd58256398acf23056a1011b01
requires.io <support@requires.io>
2020-12-26 07:14:25 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                       'requests==2.25.1',
                       'SQLAlchemy==1.3.22',
                       'suds==0.4',
-                      'rpyc==4.1.5'],
+                      'rpyc==5.0.0'],
     extras_require={
         'development':  ["pytest", "mock"]
         #'webapp': [frontend stuff],

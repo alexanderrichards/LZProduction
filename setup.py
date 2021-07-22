600cc741fe200a7cc8a2da5e62d3b4f477be0661
requires.io <support@requires.io>
2021-07-22 05:22:37 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
                       'pylru==1.2.0',
                       'PyMySQL==1.0.2',
                       'requests==2.26.0',
-                      'SQLAlchemy==1.4.21',
+                      'SQLAlchemy==1.4.22',
                       'suds==0.4',
                       'rpyc==5.0.1'],
     extras_require={
